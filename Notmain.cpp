d36051a22d8dd9b57beb4acd9a51d3ee96d74f1d
pnu-2020102741 <2020102741@pnu.edu.ru>
2022-02-17 21:38:21 +1000

    Added cout statement 1

--- Notmain.cpp
+++ Notmain.cpp
@@ -1,6 +1,11 @@
 //Notmain.cpp - implements function a_plus_b
 
+#include <iostream>
+
+using namespace std;
+
 double a_plus_b(double a, double b)
 {
+	cout << "This is the function" << endl;
 	return a + b;
 }
